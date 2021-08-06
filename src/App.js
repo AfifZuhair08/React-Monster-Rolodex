dd7a3df34d0f22b1d118d14e07167f3b261384d9
Afif Zuhair <afifzuhair08@gmail.com>
2021-08-06 23:47:42 +0800

    Create search fields component which controls the monster array by filtering it everytimes the input value changing

--- src/App.js
+++ src/App.js
@@ -1,30 +1,46 @@
 import React, { Component } from 'react';
 // import logo from './logo.svg';
 import './App.css';
-import {CardList} from './components/card-list/card-list.component'
+import { CardList } from './components/card-list/card-list.component'
 
 class App extends Component {
   constructor() {
     super();
 
     this.state = {
-      monsters: []
+      monsters: [],
+      searchFields: "",
     };
   }
 
   // fetch external data and set current state accordingly
   componentDidMount() {
     fetch("https://jsonplaceholder.typicode.com/users")
-    .then(response => response.json())
-    .then(users => this.setState({monsters: users}))
+      .then(response => response.json())
+      .then(users => this.setState({ monsters: users }))
   }
 
   // pass data to the component to render
   render() {
+
+    // destructuring state objects
+    const { monsters, searchFields } = this.state;
+    
+    // filter the array by referring to search fields everytimes its changing
+    const filteredMonsters = monsters.filter(monster =>
+      monster.name.toLowerCase().includes(searchFields.toLowerCase())
+    );
+
     return (
       <div className="App">
-      {/* Props and children props */}
-      <CardList monsters={this.state.monsters} />
+        <input
+          // synthetic event (re-render everytimes fields input is changing)
+          onChange={e => this.setState({ searchFields: e.target.value })}
+          type="search"
+          placeholder="search monster by name"
+        />
+        {/* Props and children props */}
+        <CardList monsters={filteredMonsters} />
       </div>
     )
   }
